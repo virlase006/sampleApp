f57cdb78ae807228b65d6be301831dfd688a3787
virlase006 <virla.devi@gmail.com>
2019-01-28 16:04:07 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -47,7 +47,7 @@ Write-Host "Cluster created"  #>
 az aks get-credentials --resource-group $RESOURCEGROUP --name $CLUSTERNAME --subscription 0658c1e6-74c9-4311-8207-62c4b33c5f10
 Write-Host "Fetched credentials" 
 az aks install-cli
-setx PATH "$env:path;C:\Users\VssAdministrator\.azure-kubectl" -m
+setx PATH "$env:path;C:\Users\VssAdministrator\.azure-kubectl"  /M
 Write-Host $env:path
 kubectl create namespace $NAMESPACE
 
